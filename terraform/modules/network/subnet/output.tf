eae503db71f46a33b50f94ec8f785cae10f175e8
2ghrms <aa020228@gmail.com>
2025-08-02 01:20:33 +0900

    initial setting (EC2, RDS, S3)

--- terraform/modules/network/subnet/output.tf
+++ terraform/modules/network/subnet/output.tf
@@ -1,5 +1,5 @@
 output "subnet_id"       { value = aws_subnet.this.id }
 
 output "route_table_id" {
-  value = var.gateway_id != "" ? aws_route_table.public_rt.id : null
+  value = aws_route_table.public_rt != null ? aws_route_table.public_rt.id : null
 }
