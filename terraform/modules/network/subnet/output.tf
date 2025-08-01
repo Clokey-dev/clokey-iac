9df1436504c90ec15808a56be88be45bb4741643
2ghrms <aa020228@gmail.com>
2025-08-02 01:23:43 +0900

    initial setting (EC2, RDS, S3)

--- terraform/modules/network/subnet/output.tf
+++ terraform/modules/network/subnet/output.tf
@@ -1,5 +1,7 @@
 output "subnet_id"       { value = aws_subnet.this.id }
 
 output "route_table_id" {
-  value = try(length(aws_route_table.public_rt) > 0 ? aws_route_table.public_rt[0].id : null, null)
+  value = (
+    can(aws_route_table.public_rt[0]) ? aws_route_table.public_rt[0].id : null
+  )
 }
