e6bb3b3b58d6b61f2a5218c88cfaba4cc7415e83
2ghrms <aa020228@gmail.com>
2025-09-22 02:32:37 +0900

    feat/init: S3 백엔드 적용

--- terraform/env/dev/backend.tf
+++ terraform/env/dev/backend.tf
@@ -1,7 +1,7 @@
 # S3 Backend Configuration
 terraform {
   backend "s3" {
-    bucket  = "clokey-terraform-state-116541188992"
+    bucket  = "clokey-terraform-state-dev"
     key     = "dev/terraform.tfstate"
     region  = "ap-northeast-2"
     encrypt = true
