9dd026462d9c66f7f6efb1a8940ba82dbd524af7
2ghrms <aa020228@gmail.com>
2025-10-14 01:23:28 +0900

    feat/init: userdata base64 인코딩

--- terraform/modules/compute/ec2/main.tf
+++ terraform/modules/compute/ec2/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "this" {
   monitoring                           = var.monitoring
 
   # 사용자 데이터 설정 (변수로 주입받거나 기본 파일 사용)
-  user_data                   = var.user_data != null ? var.user_data : file("${path.module}/userdata.sh")
+  user_data_base64            = var.user_data != null ? base64encode(var.user_data) : null
   user_data_replace_on_change = true # UserData 변경 시 인스턴스 교체
 
   # 루트 볼륨 설정
